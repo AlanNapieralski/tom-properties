b932c04c985059d64fcda002c4e7988f6f4abdf2
Alan Napieralski <alannap@Mac.home>
2025-01-09 10:47:08 +0000

    added places api and included it in the property form

---
+++ src/models/content/valueProperty-content.ts
@@ -0,0 +1,30 @@
+import { ComboboxItem } from "@/components/ui/Combobox"
+
+export type ImageProps = {
+  src: string,
+  alt: string,
+  width?: number,
+  height?: number
+}
+
+export const valuePropertyImage: ImageProps = {
+  src: "/assets/images/value-property.jpg",
+  alt: 'properties',
+  width: 2400,
+  height: 1350
+}
+
+export const title = 'Request an Valuation'
+export const addresses = ['123 Baker Stree flakjsdl;kfjaslk;djf;laksdjfsd jklajfklasdj;lfkaj sd;lfkjft,  London,  NW1 6XE,  United Kingdom'].map(val => ({ label: val, value: val }))
+
+const propertyTypesOptions = [
+  'example'
+]
+const valuationTypesOptions = [
+  'example'
+]
+const noOfBedsOptions = ['studio', '1', '2', '3', '4', '5', '6+']
+
+export const propertyTypes: ComboboxItem[] = propertyTypesOptions.map(val => ({ label: val, value: val }))
+export const valuationTypes: ComboboxItem[] = valuationTypesOptions.map(val => ({ label: val, value: val }))
+export const noOfBeds: ComboboxItem[] = noOfBedsOptions.map(val => ({ label: val, value: val }))
